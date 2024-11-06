7b7dba378e2e1962b28c00f92db705b8fe14685d
安玉 <glepooek@outlook.com>
2024-11-06 20:55:07 +0800

    event to command

--- Avalonia.MusicStore/Views/MainWindow.axaml.cs
+++ Avalonia.MusicStore/Views/MainWindow.axaml.cs
@@ -8,23 +8,16 @@ namespace Avalonia.MusicStore.Views
 {
     public partial class MainWindow : Window
     {
-        MainWindowViewModel viewModel;
         public MainWindow()
         {
             InitializeComponent();
-            this.Loaded += MainWindow_Loaded;
             this.Unloaded += MainWindow_Unloaded;
         }
 
         private void MainWindow_Unloaded(object? sender, Interactivity.RoutedEventArgs e)
         {
+            MainWindowViewModel viewModel = this.DataContext as MainWindowViewModel;
             WeakReferenceMessenger.Default.UnregisterAll(viewModel);
-        }
-
-        private void MainWindow_Loaded(object? sender, Interactivity.RoutedEventArgs e)
-        {
-            viewModel = this.DataContext as MainWindowViewModel;
-            viewModel?.LoadAlbums();
         }
     }
 }
