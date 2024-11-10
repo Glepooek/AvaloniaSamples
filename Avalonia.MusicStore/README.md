e319fda440c7f8f465499310a274c96e493b4e1e
安玉 <glepooek@outlook.com>
2024-11-10 19:07:49 +0800

    ControlTheme类似于WPF中的Style

---
+++ Avalonia.MusicStore/README.md
@@ -0,0 +1,12 @@
+### Avalonia与WPF对比，没有的功能：
+- 依赖项属性没有属性更改回调
+- 没有触发器（属性触发器、数据触发器等）
+- 没有Popup，类似控件Flyout
+
+### Avalonia与WPF对比，功能上不同：
+- 样式资源放在顶级元素的Styles中
+- 数据模版放在顶级元素的DataTemplates中；数据模版选择器实现IDataTemplate
+- Style类似于CSS中的样式实现，ControlTheme类似于WPF中的Style
+- 依赖项属性、附加属性定义用AvaloniaProperty.Register，类型StyledProperty
+- 路由事件的定义用RoutedEvent.Register，类型RoutedEvent<RoutedEventArgs>
+- Image的Source不能加载http\https\file://资源
--- Avalonia.MusicStore/Views/MusicStoreView.axaml.cs
+++ Avalonia.MusicStore/Views/MusicStoreView.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace Avalonia.MusicStore.Views;
 
@@ -9,5 +11,31 @@ public partial class MusicStoreView : UserControl
     public MusicStoreView()
     {
         InitializeComponent();
+    }
+
+    // 样式化属性、附加属性
+    public static readonly StyledProperty<int> RepeatCountProperty =
+        AvaloniaProperty.Register<MusicStoreView, int>(nameof(RepeatCount), defaultValue: 1);
+
+    public int RepeatCount
+    {
+        get => GetValue(RepeatCountProperty);
+        set => SetValue(RepeatCountProperty, value);
+    }
+
+    // 路由事件及引发
+    public static readonly RoutedEvent<RoutedEventArgs> ValueChangedEvent =
+        RoutedEvent.Register<MusicStoreView, RoutedEventArgs>(nameof(ValueChanged), RoutingStrategies.Bubble);
+
+    public event EventHandler<RoutedEventArgs> ValueChanged
+    {
+        add => AddHandler(ValueChangedEvent, value);
+        remove => RemoveHandler(ValueChangedEvent, value);
+    }
+
+    private void RaiseEvent()
+    {
+        RoutedEventArgs args = new RoutedEventArgs(ValueChangedEvent, this);
+        RaiseEvent(args);
     }
 }
